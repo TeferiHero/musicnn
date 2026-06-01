c3af1d5d44b95dbea4016a86102833dc2a94c4e4
sajmon <szymon17099@gmail.com>
2026-06-01 13:15:28 +0200

    Add model pruning

--- pruning.py
+++ pruning.py
@@ -3,6 +3,8 @@ import numpy as np
 import librosa
 import keras
 import tensorflow as tf
+import tensorflow_model_optimization as tfmot
+prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
 
 from musicnn.v2 import models
 from musicnn.v2 import configuration as config
@@ -20,7 +22,44 @@ def prune():
     model = tf.keras.saving.load_model("modelv2-pure.keras")
     unfreeze(model)
 
-    model.summary()
+    pruned_model = tf.keras.models.clone_model(model, clone_function=apply_pruning)
+
+    pruned_model.summary()
+
+    pruned_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
+              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['accuracy'])
+
+    callbacks = [
+        tfmot.sparsity.keras.UpdatePruningStep()
+    ]
+
+    train_ds, val_ds, test_ds = get_dataset(augument_training=True, all_noise_ratio=0.25)
+    pruned_model.fit(
+        train_ds,
+        epochs=3,
+        callbacks=callbacks
+    )
+    
+    stripped_pruned_model = tfmot.sparsity.keras.strip_pruning(pruned_model)
+    stripped_pruned_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
+              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['accuracy'])
+    
+    print(stripped_pruned_model.evaluate(test_ds))
+    stripped_pruned_model.save("stripped_pruned_model.keras")
+
+
+def apply_pruning(layer):
+    pruning_params = {
+        'pruning_schedule': tfmot.sparsity.keras.ConstantSparsity(0.3, begin_step=0, frequency=100)
+    }
+    
+    if isinstance(layer, tf.keras.layers.Conv2D) \
+       or isinstance(layer, tf.keras.layers.Dense) and layer.activity_regularizer is None:
+        return prune_low_magnitude(layer, **pruning_params)
+
+    return layer
 
 
 def unfreeze(model):
